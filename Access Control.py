88136ae5c40b92107c49aead0ae23260c54418d4
Kayaroganam V <88090401+Kayaroganam@users.noreply.github.com>
2022-02-26 14:40:26 +0530

    Update Access Control.py

--- Access Control.py
+++ Access Control.py
@@ -9,7 +9,6 @@ def main_help():
 main_help()
 while True:
 	select = input("access control >> ")
-
 	if select == '0':
 		print("BYE\n")
 		break
@@ -22,18 +21,17 @@ while True:
 		others_perm = input(f"access control > [{selected_file}] > others: ")
 
 		if owner_perm != '':
-			os.system(f"sudo chmod u-rwx {selected_file}")
+			os.system("sudo chmod u-rwx")
 			os.system(f"sudo chmod u+{owner_perm} {selected_file}")
 			pass
 		if group_perm != '':
-			os.system(f"sudo chmod g-rwx {selected_file}")
+			os.system("sudo chmod g-rwx")
 			os.system(f"sudo chmod g+{group_perm} {selected_file}")
 			pass
 		if others_perm != '':
-			os.system(f"sudo chmod o-rwx {selected_file}")
+			os.system("sudo chmod o-rwx")
 			os.system(f"sudo chmod o+{others_perm} {selected_file}")
 			pass
-
 		break
 
 	elif select == '2':
@@ -41,15 +39,11 @@ while True:
 		selected_file = input("access control > Enter File or Directory name: ")
 		selected_user = input("access control > Enter user name: ")
 		os.system(f"sudo chown {selected_user} {selected_file}")
-		break
-
+		break	
 	elif select == '3':
 		os.system("ls")
 		selected_file = input("access control > Enter File or Directory name: ")
 		selected_group = input("access control > Enter group name: ")
-		os.system(f"sudo chgrp {selected_group} {selected_file}")
 		break
-
-
 	elif select == 'help' or select == 'HELP' or select == 'Help' or select == 'h' or select == 'H':
 		main_help()
